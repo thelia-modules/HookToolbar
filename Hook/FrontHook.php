1a4ebdefe1b5d4337d727d9259f94e0a1a2552d7
Michaël Espeche <michael.espeche@gmail.com>
2015-01-28 20:58:54 +0100

    Init repository

---
+++ Hook/FrontHook.php
@@ -0,0 +1,31 @@
+<?php
+/*************************************************************************************/
+/*      This file is part of the Thelia package.                                     */
+/*                                                                                   */
+/*      Copyright (c) OpenStudio                                                     */
+/*      email : dev@thelia.net                                                       */
+/*      web : http://www.thelia.net                                                  */
+/*                                                                                   */
+/*      For the full copyright and license information, please view the LICENSE.txt  */
+/*      file that was distributed with this source code.                             */
+/*************************************************************************************/
+
+namespace HookToolbar\Hook;
+use Thelia\Core\Event\Hook\HookRenderEvent;
+use Thelia\Core\Hook\BaseHook;
+
+
+/**
+ * Class FrontHook
+ * @package HookToolbar\Hook
+ * @author Michaël Espeche <michael.espeche@gmail.com>
+ */
+class FrontHook extends BaseHook {
+
+    public function onMainBodyTop(HookRenderEvent $event)
+    {             
+        $content = $this->render("main-body-top.html");
+        $event->add($content);
+    }
+
+} 
