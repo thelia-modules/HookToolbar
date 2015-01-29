23003b9a6871e1b7063e5294a849275a426bd0ee
Michaël Espeche <michael.espeche@gmail.com>
2015-01-29 19:21:40 +0100

    Add translations

---
+++ I18n/frontOffice/default/en_US.php
@@ -0,0 +1,23 @@
+<?php
+
+return array(
+    'Customers' => 'Customers',
+    'Orders' => 'Orders',
+    'Catalog' => 'Catalog',
+    'Folders' => 'Folders',
+    'Tools' => 'Tools',
+    'Coupons' => 'Coupons',
+    'Sales management' => 'Sales management',
+    'Brands' => 'Brands',
+    'Export' => 'Export',
+    'Import' => 'Import',
+    'Modules' => 'Modules',
+    'Configuration' => 'Configuration',
+    'Update category' => 'Update category',
+    'Update product' => 'Update product',
+    'Update folder' => 'Update folder',
+    'Update content' => 'Update content',
+    'Hi, %name' => 'Hi, %name',
+    'Profile' => 'Profile',
+    'Logout' => 'Logout'    
+);
---
+++ I18n/frontOffice/default/fr_FR.php
@@ -0,0 +1,23 @@
+<?php
+
+return array(
+    'Customers' => 'Clients',
+    'Orders' => 'Commandes',
+    'Catalog' => 'Catégories',
+    'Folders' => 'Dossiers',
+    'Tools' => 'Outils',
+    'Coupons' => 'Codes promo',
+    'Sales management' => 'Gestion des promotions',
+    'Brands' => 'Marques et fournisseurs',
+    'Export' => 'Export',
+    'Import' => 'Import',
+    'Modules' => 'Modules',
+    'Configuration' => 'Configuration',
+    'Update category' => 'Modifier la catégorie',
+    'Update product' => 'Modifier le produit',
+    'Update folder' => 'Modifier le dossier',
+    'Update content' => 'Modifier le contenu',
+    'Hi, %name' => 'Bonjour, %name',
+    'Profile' => 'Profil',
+    'Logout' => 'Déconnexion'
+);
