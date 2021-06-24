9e89b0d2dd471c42dbce55c3ed87da7b4cfed68a
Nicolas Barbey <223106@supinfo.com>
2021-06-24 11:57:34 +0200

    Thelia 2.5 compatibility

--- HookToolbar.php
+++ HookToolbar.php
@@ -12,9 +12,17 @@
 
 namespace HookToolbar;
 
+use Symfony\Component\DependencyInjection\Loader\Configurator\ServicesConfigurator;
 use Thelia\Module\BaseModule;
 
 class HookToolbar extends BaseModule
 {
 
+    public static function configureServices(ServicesConfigurator $servicesConfigurator): void
+    {
+        $servicesConfigurator->load(self::getModuleCode().'\\', __DIR__)
+            ->exclude([THELIA_MODULE_DIR . ucfirst(self::getModuleCode()). "/I18n/*"])
+            ->autowire(true)
+            ->autoconfigure(true);
+    }
 }
